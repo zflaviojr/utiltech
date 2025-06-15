a53adde3d87a473eae0fdb11ff70319716f53f76
José Flávio <zflavio@ufcg.edu.br>
2025-06-15 17:33:32 -0300

    Ajustes na lista de compras

--- script.js
+++ script.js
@@ -686,8 +686,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div class="col-span-10  p-4 ">
                         <!-- dividir internamente entre duas grandes linhas, superior e inferior, onde a superior ficará um texto apenas, e na segunda linha, três colunas lado a lado-->
                         <p class="text-lg font-bold"><span class="text-base">${item.name}</span></p>
-                        <div class="grid grid-cols-3 gap-0 items-center  justify-center">
-                            <div class="p-4">
+                        <div class="grid grid-cols-3 gap-0 flex flex-row justify-between items-start flex-nowra">
+                            <div class="p-4 ">
                                 <!--p class="text-gray-600"><span class="text-sm">R$</span -->
                                 <input type="number" min="0" value="${parseFloat(item.price).toFixed(2)}" data-index="${index}" class="edit-price bg-transparent border-b border-gray-300 w-20 text-sm text-right focus:outline-none focus:border-primary" step="0.01" style="appearance: textfield;"/>
                             </div>
